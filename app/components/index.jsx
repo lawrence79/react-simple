f5b6c554d573ab1ad95486079847f210b65ef79b
Lawrence Norton <lawrence@lawrencenorton.com>
2016-01-08 09:36:48 -0500

    working on comments

--- app/components/index.jsx
+++ app/components/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
 import SweetComponent from './SweetComponent.jsx';
+import CommentBox from './CommentBox.jsx';
+
+// static data
+let data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
 
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <p> Hello React!</p>
-        <SweetComponent />
-      </div>
+       <div className="container-fluid">
+          <div className="col-md-12">
+            <h1> Hello React!</h1>
+            <SweetComponent />
+            <CommentBox data={data} />
+          </div>
+        </div>
     );
   }
 }
