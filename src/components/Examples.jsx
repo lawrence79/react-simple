7519fa050eef9fe369db15e5472df55c7692a60f
Lawrence Norton <lawrence@lawrencenorton.com>
2016-04-18 13:51:23 -0400

    cleanup

---
+++ src/components/Examples.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {render} from 'react-dom';
+
+
+export class Examples extends React.Component {
+  render () {
+    return <div class="container">This is the examples container</div>;
+  }
+}
+
