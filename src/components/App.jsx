dfa7a4d56bbf9fb48f6a084e731b3eb174c60721
Lawrence Norton <lawrence@lawrencenorton.com>
2016-04-21 12:36:40 -0400

    updating

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import uuid from 'node-uuid';
+import React from 'react';
+
+import Notes from './Notes.jsx';
+
+export default class App extends React.Component {
+    constructor(props)  {
+        super(props);
+
+        this.state = {
+            notes: [{
+                id: uuid.v4(),
+                task: 'Learn Webpack'
+            }, {
+                id: uuid.v4(),
+                task: 'Learn React'
+            }, {
+                id: uuid.v4(),
+                task: 'Do laundry'
+            }]
+        };
+        this.addNote = this.addNote.bind(this)
+    }
+    addNote() {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New task'
+            }])
+        });
+    }
+
+    render() {
+        const notes = this.state.notes;
+
+        return (
+          <div>
+            <button onClick={this.addNote}>+</button>
+            <Notes notes={notes} />
+          </div>
+        );
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,79 @@
-var path = require('path');
-var webpack = require('webpack');
+'use strict';
 
-module.exports = {
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  public: path.join(__dirname, 'public')
+};
+
+const common = {
+  entry: [
+    PATHS.src + '/index.jsx'
+  ],
+  output: {
+    path: PATHS.public,
+    filename: 'bundle.js'
+  },
   module: {
     loaders: [
       {
-        loader: "babel-loader",
-        // Skip any files outside of your project's `src` directory
-        include: [
-          path.resolve(__dirname, "src"),
-        ],
-        // Only run `.js` and `.jsx` files through Babel
+        // Test expects a RegExp! Note the slashes!
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.src
+      },
+      {
         test: /\.jsx?$/,
-        // Options to configure babel with
-        query: {
-          plugins: ['transform-runtime'],
-          presets: ['es2015', 'react'],
-        }
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        include: PATHS.src
       }
     ]
   },
-  output: {
-    filename: 'bundle.js'
-  },
-  entry: [
-    './src/index.js'
-  ]
+   resolve: {
+    extensions: ['', '.js', '.jsx'],
+  }
 };
+
+
+if(TARGET === 'dev' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.public,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST,
+      port: process.env.PORT || 3000
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
